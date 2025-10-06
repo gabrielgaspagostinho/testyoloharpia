6919677705a00148e859e8a96e7adb7ad77b62cd
Gabriel Gaspar Agostinho <119149850+gabrielgaspagostinho@users.noreply.github.com>
2025-10-06 19:53:06 -0300

    Update train.py

--- train.py
+++ train.py
@@ -11,18 +11,11 @@ def main():
         name='yolov8n_platform_detector',
         batch=16,
         workers=4,
-        # --- PARÂMETROS DE DATA AUGMENTATION ---
-        degrees=15.0,  # Variação aleatória na rotação (em graus)
-        translate=0.1,  # Variação aleatória na translação (fração da imagem)
-        scale=0.1,  # Variação aleatória no zoom/escala
-        fliplr=0.5,  # 50% de chance de espelhar a imagem horizontalmente
-        hsv_h=0.015,  # Variação na Matiz (Hue) da cor
-        hsv_s=0.7,  # Variação na Saturação da cor
-        hsv_v=0.4  # Variação no Brilho (Value) da cor
     )
 
     print("Treinamento concluído!")
     print(f"Resultados salvos em: {results.save_dir}")
 
 if __name__ == '__main__':
+
     main()
